b1d141ce8fdea8e17c4f719653d7fdece09d401f
LadyMartel <LadyMartel@gmail.com>
2014-06-26 11:24:25 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -20,9 +20,8 @@
   CCPhysicsJoint *_penguinCatapultJoint;
 }
 
-// is called when CCB file has completed loading
 - (void)didLoadFromCCB {
-  // tell this scene to accept touches
+  
   self.userInteractionEnabled = TRUE;
   CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
   [_levelNode addChild:level];
@@ -38,7 +37,6 @@
 // called on every touch in this scene
 -(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
-  
   CGPoint touchLocation = [touch locationInNode:_contentNode];
   
   // start catapult dragging when a touch inside of the catapult arm occurs
